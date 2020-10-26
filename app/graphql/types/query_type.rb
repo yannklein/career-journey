199ed833bea431f5b6ea62048ce85d1148dba653
Yann Klein <yann.klein@me.com>
2020-10-26 19:53:42 +0900

    add users query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,24 +1,19 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
-  end
-
-  class QueryType < Types::BaseObject
     field :steps,
           [Types::StepType],
           null: false,
           description: "Returns the list of career journey steps"
+    field :users,
+          [Types::UserType],
+          null: false,
+          description: "Returns the list of users"
 
     def steps
       Step.all
+    end
+    def users
+      User.all
     end
   end
 end
---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,17 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String, null: false
+    field :encrypted_password, String, null: false
+    field :reset_password_token, String, null: true
+    field :reset_password_sent_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :remember_created_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :step_id, Integer, null: true
+    field :name, String, null: true
+    field :batch, Integer, null: true
+    field :github_account, String, null: true
+    field :step, Types::StepType, null: false
+  end
+end
