36f22a1a5b164bf533db858eec2920d916e3a346
Yann Klein <yann.klein@me.com>
2020-11-02 15:48:35 +0900

    created a base sidebar

---
+++ app/javascript/sideBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+
+  name: 'sideBar',
+  data(){
+    return{
+        steps:''
+    }
+  },
+  apollo:{
+    steps:{
+      query: gql`
+        query {
+          steps{
+            id
+            step_number
+            title
+          }
+        }
+      `,
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
