5547275a9f3adbbea80f11ef36ced989ac8e45fe
Yann Klein <yann.klein@me.com>
2020-11-02 15:45:38 +0900

    added a sort function in the steps query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,16 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
-    field :steps,
-          [Types::StepType],
-          null: false,
-          description: "Returns the list of career journey steps"
+    field :steps, [Types::StepType], null: false do
+      description "Returns the list of career journey steps"
+      argument :order_by, String, required: false
+    end
     field :users,
           [Types::UserType],
           null: false,
           description: "Returns the list of users"
 
-    def steps
-      Step.all
+    def steps(order_by:)
+      # order_by = {step_number: :desc}
+      order_by ? Step.order(order_by) : Step.all
     end
     def users
       User.all
