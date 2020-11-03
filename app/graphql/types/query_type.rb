6d3d89c4411d53b130b370dee697560399f59c19
Yann Klein <yann.klein@me.com>
2020-11-03 13:42:58 +0900

    new one step query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,9 +4,11 @@ module Types
     field :steps, [Types::StepType], null: false do
       description "Returns the list of career journey steps"
       argument :order_by, String, required: false
-      # resolve ->(obj, args, context) {
-      #   context[:current_user].steps
-      # }
+    end
+
+    field :step, Types::StepType, null: false do
+      description "Returns one career journey steps"
+      argument :stepId, Integer, required: true
     end
 
     field :users, [Types::UserType], null: false do
@@ -19,6 +21,10 @@ module Types
       # order_by = {step_number: :desc}
       # context[:current_user].steps
       order_by ? Step.order(order_by) : Step.all
+    end
+
+    def step(stepId: 0)
+      Step.find(stepId)
     end
 
     def users
