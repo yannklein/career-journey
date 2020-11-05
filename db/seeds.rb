824e0a64509886e1af434eaca30015c209becf24
Yann Klein <yann.klein@me.com>
2020-11-05 19:25:33 +0900

    added steps into mn files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Resource.destroy_all
 Step.destroy_all
@@ -18,20 +11,12 @@ resource_type = {
   other: "other"
 }
 
+steps_path = Rails.root.join('db/steps')
+
 puts "Steps/Resource creation"
 step1 = Step.create(
   title: "CV and Linkedin",
-  description:
-    "As discussed, today is the first pre-Demo day Career Week challenge!
-
-## The objectives ☕️
-
-- Build your CV - follow the slides for best practices 💅
-- Refine your LinkedIn - follow the slides to get the best reach 💼
-- Read the Career Playbook
-- Post your LinkedIn profile on the batch Slack channel, add your classmates and go to their profile to endorse them - you guys are more than qualified to be endorsed in Ruby on Rails, Git, JavaScript, HTML/CSS, etc..  it only takes 3-5 clicks and will add more legitimacy to your profile as a developer.
-
-Write some recommendations for your classmates too! You can ask someone to recommend you, but make sure you write one back if someone writes one for you.",
+  description: File.open("#{steps_path}/step1.md"),
   step_number: 1
 )
 
@@ -61,18 +46,7 @@ Resource.create(
 
 step2 = Step.create(
   title: "Portfolio",
-  description:
-    "Today, we focus on building your portfolio!
-
-## Our plan 📝
-
-- Assemble a list of all of the social links you want to link on your portfolio site: your Github (pimp it with our tuto 👇), Medium, LinkedIn, Instagram, etc.
-- Pick a domain name for your site and purchase it. Useful tips: use a .com, .me, .tech, or a .io domain. Do not use .co as it often gets flagged as spam
-- Fork 🍴 your Final projects, your Airbnb projects, Mr. Cocktail, any others and clean up the front end.
-- Take a screenshot of your products’ landing page.
-- Do a one paragraph write-up describing the challenges and solutions of the project. This can be as simple as the product card on the Kitt products page
-
-_For bonus points:_ Take a video of your journey to upload to your site (this will be useful to share with potential clients too!)",
+  description: File.open("#{steps_path}/step2.md"),
   step_number: 2
 )
 
@@ -100,37 +74,15 @@ Resource.create(
   step: step2
 )
 
-step2 = Step.create(
+step3 = Step.create(
   title: "Landing your first job",
-  description:
-    "Today, we focus on building your portfolio!
-
-## Our plan 📝
-
-- Assemble a list of all of the social links you want to link on your portfolio site: your Github (pimp it with our tuto 👇), Medium, LinkedIn, Instagram, etc.
-- Pick a domain name for your site and purchase it. Useful tips: use a .com, .me, .tech, or a .io domain. Do not use .co as it often gets flagged as spam
-- Fork 🍴 your Final projects, your Airbnb projects, Mr. Cocktail, any others and clean up the front end.
-- Take a screenshot of your products’ landing page.
-- Do a one paragraph write-up describing the challenges and solutions of the project. This can be as simple as the product card on the Kitt products page
-
-_For bonus points:_ Take a video of your journey to upload to your site (this will be useful to share with potential clients too!)",
+  description: File.open("#{steps_path}/step2.md"),
   step_number: 3
 )
 
-step3 = Step.create(
+step4 = Step.create(
   title: "Tech interview & coding after Le Wagon",
-  description:
-    "Today, we focus on building your portfolio!
-
-## Our plan 📝
-
-- Assemble a list of all of the social links you want to link on your portfolio site: your Github (pimp it with our tuto 👇), Medium, LinkedIn, Instagram, etc.
-- Pick a domain name for your site and purchase it. Useful tips: use a .com, .me, .tech, or a .io domain. Do not use .co as it often gets flagged as spam
-- Fork 🍴 your Final projects, your Airbnb projects, Mr. Cocktail, any others and clean up the front end.
-- Take a screenshot of your products’ landing page.
-- Do a one paragraph write-up describing the challenges and solutions of the project. This can be as simple as the product card on the Kitt products page
-
-_For bonus points:_ Take a video of your journey to upload to your site (this will be useful to share with potential clients too!)",
+  description: File.open("#{steps_path}/step2.md"),
   step_number: 4
 )
 
---
+++ db/steps/step1.md
@@ -0,0 +1,10 @@
+As discussed, today is the first pre-Demo day Career Week challenge!
+
+## The objectives ☕️
+
+- Build your CV - follow the slides for best practices 💅
+- Refine your LinkedIn - follow the slides to get the best reach 💼
+- Read the Career Playbook
+- Post your LinkedIn profile on the batch Slack channel, add your classmates and go to their profile to endorse them - you guys are more than qualified to be endorsed in Ruby on Rails, Git, JavaScript, HTML/CSS, etc..  it only takes 3-5 clicks and will add more legitimacy to your profile as a developer.
+
+Write some recommendations for your classmates too! You can ask someone to recommend you, but make sure you write one back if someone writes one for you.
