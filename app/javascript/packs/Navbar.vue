8f52cf17716515119e392b4323095101a424675e
Yann Klein <yann.klein@me.com>
2020-11-13 16:49:41 +0900

    clean up navbar style and added a font

--- app/javascript/packs/Navbar.vue
+++ app/javascript/packs/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="navbar-vanilla">
-    <a class="navbar-vanilla-section" href="#">
-      <img class="navbar-vanilla-logo" src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png" alt="lewagon">
-      <h2 class="navbar-vanilla-brand">Your career Journey</h2>
+  <div class="navbar">
+    <a class="navbar-section" href="#">
+      <img class="navbar-logo" src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png" alt="lewagon">
+      <h2 class="navbar-brand">Your career Journey</h2>
     </a>
-    <div class="navbar-vanilla-section">
-      <div class="navbar-vanilla-menu">
+    <div class="navbar-section">
+      <div class="navbar-menu">
         <a href="#">Current step</a>
         <a href="#">Contact us</a>
       </div>
-      <div class="navbar-vanilla-profile" v-on:click="openMenu($event)">
+      <div class="navbar-profile" v-on:click="openMenu($event)">
         <img src="https://avatars2.githubusercontent.com/u/26819547?s=400&amp;u=ae79d8825ad1127723641cbf32a9a7e2ec221e7f&amp;v=4" alt="">
         <ul>
           <li><a href="#">To Kitt</a></li>
@@ -41,7 +41,7 @@ export default {
 </script>
 
 <style lang="css" scoped>
-.navbar-vanilla {
+.navbar {
   display: flex;
   height: 96px;
   align-items: center;
@@ -49,47 +49,48 @@ export default {
   background-color: white;
 }
 
-.navbar-vanilla a {
+.navbar a {
   text-decoration: none;
   color: rgb(40,40,40);
   font-size: 20px;
 }
 
-.navbar-vanilla-section {
+.navbar-section {
   display: flex;
   align-items: center;
 }
 
-.navbar-vanilla-logo {
+.navbar-logo {
   height: 64px;
   width: 64px;
   object-fit: cover;
   object-position: 50% 50%;
-  margin: 0 32px;
+  margin-left: 32px;
+  margin-right: 16px;
 }
 
-.navbar-vanilla-brand {
+.navbar-brand {
   margin: 0;
   font-size: 24px;
 }
 
-.navbar-vanilla-menu a {
+.navbar-menu a {
   margin: 0 16px;
 }
 
-.navbar-vanilla-profile {
+.navbar-profile {
   position: relative;
   margin: 0 32px;
 }
 
-.navbar-vanilla-profile img {
+.navbar-profile img {
   width: 64px;
   height: 64px;
   border-radius: 50%;
   cursor: pointer;
 }
 
-.navbar-vanilla-profile ul {
+.navbar-profile ul {
   position: absolute;
   top: 64px;
   right: 0;
@@ -106,27 +107,27 @@ export default {
   transition: 0.3s;
 }
 
-.navbar-vanilla-profile li a{
+.navbar-profile li a{
   margin: 4px;
   font-size: 16px;
 }
 
-.navbar-vanilla-profile.active ul {
+.navbar-profile.active ul {
   opacity: 1;
   visibility: visible;
 }
 
 /* Small devices (portrait tablets and large phones, 600px and up) */
 @media (max-width: 576px) {
-  .navbar-vanilla-brand {
+  .navbar-brand {
     font-size: 24px;
   }
 
-  .navbar-vanilla-menu {
+  .navbar-menu {
     display: none;
   }
 
-  .navbar-vanilla-profile ul {
+  .navbar-profile ul {
 
   }
 }
