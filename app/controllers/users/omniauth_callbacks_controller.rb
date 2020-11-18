c7a662705d09171071ead2a6d630462e4c4db49b
Yann Klein <yann.klein@me.com>
2020-11-18 18:34:12 +0900

    block user if not from a LW tokyo batch

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,7 +9,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def all
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
+    if @user.persisted? && @user.batch != 0
       flash.notice = "Signed in!"
       sign_in_and_redirect @user, :event => :authentication
     else
